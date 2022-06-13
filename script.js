96d28ecbd1016d253dfea67671a4ec5fccd4c106
Daivison Morais <daivison13pinheiro@gmail.com>
2022-06-13 17:46:15 -0300

    praticamente tudo pronto

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let primeirafileira;
 let segundafileira;
 let terceirafileira;
 
+let precoprato;
+let precobebida;
+let precosobremesa;
+let totalapagar = precoprato + precobebida + precosobremesa;
+
 function bordaverde1(elemento){
     primeirafileira = elemento.innerHTML
     const escolhaselecionada = document.querySelector(".fileira1 .bordaverde")
@@ -14,6 +19,9 @@ function bordaverde1(elemento){
          greenpedidofechado.classList.add("pedidofechado");
          greenpedidofechado.innerHTML = "Pedido Fechado";
      }
+     preco = document.querySelector(".preco")
+     precoprato = Number(preco.innerHTML).toFixed(4);
+     return precoprato
 }
 function bordaverde2(elemento){
     segundafileira = elemento.innerHTML;
@@ -27,6 +35,9 @@ function bordaverde2(elemento){
          greenpedidofechado.classList.add("pedidofechado");
          greenpedidofechado.innerHTML = "Pedido Fechado";
      }
+     preco = document.querySelector(".preco")
+     precobebida = Number(preco.innerHTML).toFixed(4);
+     return precobebida
 }
 function bordaverde3(elemento){
     terceirafileira= elemento.innerHTML
@@ -40,9 +51,25 @@ function bordaverde3(elemento){
          greenpedidofechado.classList.add("pedidofechado");
          greenpedidofechado.innerHTML = "Pedido Fechado";
      }
+     preco = document.querySelector(".preco")
+     precosobremesa = Number(preco.innerHTML).toFixed(4);
+     return precosobremesa
+     
 }
 
-
+function clickbotao(){
+    if (primeirafileira && segundafileira && terceirafileira){
+    let mensagem =
+"Olá, gostaria de fazer o pedido:\n\
+- Prato: Frango Yin Yangn\n\
+- Bebida: Coquinha Geladan\n\
+- Sobremesa: Pudim\n\
+Total: R$ " + 
+27.70; 
+link ="https://wa.me/5591989391048?text=" + encodeURIComponent(mensagem);
+window.open(link);
+    }
+}
 
 //const caixabotao = document.querySelector(".caixabotao");
 //caixabotao.innerHTML = "Pedido fechado";
