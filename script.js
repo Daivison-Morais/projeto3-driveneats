ae7ae5da1de350b54541dcc0be74ae2c8d54c040
Daivison Morais <daivison13pinheiro@gmail.com>
2022-06-12 21:27:34 -0300

    dinamização escolha e pedido fechado prontas

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-*//console.log("");       alert("");
+let primeirafileira;
+let segundafileira;
+let terceirafileira;
+
+function bordaverde1(elemento){
+    primeirafileira = elemento.innerHTML
+    const escolhaselecionada = document.querySelector(".fileira1 .bordaverde")
+    if(escolhaselecionada !== null){
+        escolhaselecionada.classList.remove("bordaverde")
+    }
+    elemento.classList.add("bordaverde")
+    if (primeirafileira && segundafileira && terceirafileira){
+        let greenpedidofechado = document.querySelector(".caixabotao");
+         greenpedidofechado.classList.add("pedidofechado");
+         greenpedidofechado.innerHTML = "Pedido Fechado";
+     }
+}
+function bordaverde2(elemento){
+    segundafileira = elemento.innerHTML;
+    const escolhaselecionada = document.querySelector(".fileira2 .bordaverde")
+    if(escolhaselecionada !== null){
+        escolhaselecionada.classList.remove("bordaverde")
+    }
+    elemento.classList.add("bordaverde")
+    if (primeirafileira && segundafileira && terceirafileira){
+        let greenpedidofechado = document.querySelector(".caixabotao");
+         greenpedidofechado.classList.add("pedidofechado");
+         greenpedidofechado.innerHTML = "Pedido Fechado";
+     }
+}
+function bordaverde3(elemento){
+    terceirafileira= elemento.innerHTML
+    const escolhaselecionada = document.querySelector(".fileira3 .bordaverde")
+    if(escolhaselecionada !== null){
+        escolhaselecionada.classList.remove("bordaverde")
+    }
+    elemento.classList.add("bordaverde")
+    if (primeirafileira && segundafileira && terceirafileira){
+        let greenpedidofechado = document.querySelector(".caixabotao");
+         greenpedidofechado.classList.add("pedidofechado");
+         greenpedidofechado.innerHTML = "Pedido Fechado";
+     }
+}
+
+
+
+//const caixabotao = document.querySelector(".caixabotao");
+//caixabotao.innerHTML = "Pedido fechado";
