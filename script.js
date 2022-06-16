9a4dd775ee838f58fe30c641fadd1e26fa05f08b
Daivison Morais <daivison13pinheiro@gmail.com>
2022-06-16 17:18:06 -0300

    completamente pronto

--- script.js
+++ script.js
@@ -1,72 +1,67 @@
-let primeirafileira;
-let segundafileira;
-let terceirafileira;
-
-let precoprato;
-let precobebida;
-let precosobremesa;
-let totalapagar = precoprato + precobebida + precosobremesa;
-
+let tituloprato, titulobebida, titulosobremesa;
+let precoprato, precobebida, precosobremesa;
+function converterNumero(stringnumber){
+    preco = stringnumber.replace(",",".")
+    preco = Number(preco) * 100
+    return preco
+}
 function bordaverde1(elemento){
-    primeirafileira = elemento.innerHTML
+    
     const escolhaselecionada = document.querySelector(".fileira1 .bordaverde")
     if(escolhaselecionada !== null){
         escolhaselecionada.classList.remove("bordaverde")
     }
     elemento.classList.add("bordaverde")
-    if (primeirafileira && segundafileira && terceirafileira){
-        let greenpedidofechado = document.querySelector(".caixabotao");
-         greenpedidofechado.classList.add("pedidofechado");
-         greenpedidofechado.innerHTML = "Pedido Fechado";
-     }
-     preco = document.querySelector(".preco")
-     precoprato = Number(preco.innerHTML).toFixed(4);
-     return precoprato
-}
-function bordaverde2(elemento){
-    segundafileira = elemento.innerHTML;
-    const escolhaselecionada = document.querySelector(".fileira2 .bordaverde")
-    if(escolhaselecionada !== null){
-        escolhaselecionada.classList.remove("bordaverde")
-    }
-    elemento.classList.add("bordaverde")
-    if (primeirafileira && segundafileira && terceirafileira){
-        let greenpedidofechado = document.querySelector(".caixabotao");
-         greenpedidofechado.classList.add("pedidofechado");
-         greenpedidofechado.innerHTML = "Pedido Fechado";
-     }
-     preco = document.querySelector(".preco")
-     precobebida = Number(preco.innerHTML).toFixed(4);
-     return precobebida
-}
-function bordaverde3(elemento){
-    terceirafileira= elemento.innerHTML
-    const escolhaselecionada = document.querySelector(".fileira3 .bordaverde")
-    if(escolhaselecionada !== null){
-        escolhaselecionada.classList.remove("bordaverde")
-    }
-    elemento.classList.add("bordaverde")
-    if (primeirafileira && segundafileira && terceirafileira){
-        let greenpedidofechado = document.querySelector(".caixabotao");
-         greenpedidofechado.classList.add("pedidofechado");
-         greenpedidofechado.innerHTML = "Pedido Fechado";
-     }
-     preco = document.querySelector(".preco")
-     precosobremesa = Number(preco.innerHTML).toFixed(4);
-     return precosobremesa
+    tituloprato = elemento.querySelector(".texto1").innerHTML
+    precoprato = elemento.querySelector(".preco").innerHTML
+    precoprato = converterNumero(precoprato)
+    console.log(precoprato, typeof(precoprato)) 
+    fazerPedido() 
      
 }
+function bordaverde2(elemento){
+    let escolhaselecionada = document.querySelector(".fileira2 .bordaverde")
+    if(escolhaselecionada !== null){
+        escolhaselecionada.classList.remove("bordaverde")
+    }
+    elemento.classList.add("bordaverde")
+    titulobebida = elemento.querySelector(".texto1").innerHTML
+    precobebida = elemento.querySelector(".preco").innerHTML
+    precobebida = converterNumero(precobebida)
+    console.log(precobebida, typeof(precobebida))
+    fazerPedido()
 
+}
+function bordaverde3(elemento){
+    let escolhaselecionada = document.querySelector(".fileira3 .bordaverde")
+    if(escolhaselecionada !== null){
+        escolhaselecionada.classList.remove("bordaverde")
+    }
+    elemento.classList.add("bordaverde")
+    titulosobremesa = elemento.querySelector(".texto1").innerHTML
+    precosobremesa = elemento.querySelector(".preco").innerHTML
+    precosobremesa = converterNumero(precosobremesa)
+    console.log(precosobremesa, typeof(precosobremesa))
+    fazerPedido()
+}
+
+function fazerPedido(){
+    if (tituloprato && titulobebida && titulosobremesa){
+        let greenpedidofechado = document.querySelector(".caixabotao");
+         greenpedidofechado.classList.add("pedidofechado");
+         greenpedidofechado.innerHTML = "PedidoFechado";
+    }
+}
 function clickbotao(){
-    if (primeirafileira && segundafileira && terceirafileira){
-    let mensagem =
-"Olá, gostaria de fazer o pedido:\n\
-- Prato: Frango Yin Yangn\n\
-- Bebida: Coquinha Geladan\n\
-- Sobremesa: Pudim\n\
-Total: R$ " + 
-27.70; 
-link ="https://wa.me/5591989391048?text=" + encodeURIComponent(mensagem);
+    if (tituloprato && titulobebida && titulosobremesa){
+    let precototal = precoprato + precobebida + precosobremesa;
+    let mensagem = `
+Olá, gostaria de fazer o pedido:
+- Prato: ${tituloprato}
+- Bebida: ${titulobebida}\n\
+- Sobremesa: ${titulosobremesa}\n\
+Total: R$ ${(precototal/100).toFixed(2)} `
+link ="https://wa.me/5591989391048?text=" + encodeURIComponent(mensagem)
 window.open(link);
     }
 }
